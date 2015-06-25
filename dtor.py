313a943fd1861469f2ea2709024568206a4a5880
PedroGabriel <pedrogabriel@protonmail.ch>
2015-06-25 18:14:19 +0100

    New class to perform the torrent file download async. New command to download the file --download.

--- dtor.py
+++ dtor.py
@@ -1,20 +1,5 @@
-class Torrent:
-    def __init__(self, id, name, size, age, seeds, leechers):
-        self.id = id
-        self.name = name
-        self.size = size
-        self.age = age
-        self.seeds = seeds
-        self.leechers = leechers
-
-    def __str__(self):
-        return self.id + " - " + self.name + " - " + self.size + " - " + self.age + " - " + self.seeds + " - " + self.leechers
-
-
-
-
 import requests, sys, bs4
-import webbrowser, argparse, collections
+import webbrowser, argparse, collections, threading
 from colors import red, green
 from prettytable import PrettyTable
 
@@ -31,12 +16,48 @@ leechers = None
 torrents_found = {}
 
 #support link
-link = 'https://kickass.to/usearch/'
+link = 'https://kat.cr/usearch/'
 #main link
 actual_link = None
 #query to get them in descending order
 query = '/?field=seeders&sorder=desc'
 
+
+class Torrent:
+    '''
+    The representation of a torrent.
+    '''
+    def __init__(self, id, name, size, age, seeds, leechers):
+        self.id = id
+        self.name = name
+        self.size = size
+        self.age = age
+        self.seeds = seeds
+        self.leechers = leechers
+
+    def __str__(self):
+        return self.id + " - " + self.name + " - " + self.size + " - " + self.age + " - " + self.seeds + " - " + self.leechers
+
+
+class AsyncDownload(threading.Thread):
+    '''
+    Class that is used to download a torrent async.
+    '''
+    def __init__(self, id_of_torrent):
+        threading.Thread.__init__(self)
+        self.id_of_torrent = id_of_torrent
+
+    def run(self):
+        try:
+            torrFile = requests.get(href[self.id_of_torrent])
+            torrFile.raise_for_status()
+        except Exception:
+            print('Download could not be completed..')
+            return
+        actualTorrentFile = open(title[self.id_of_torrent] + '.torrent', 'wb')
+        for chunk in torrFile.iter_content(100000):
+            actualTorrentFile.write(chunk)
+        print("Download Completed !")
 
 
 def entry_point(args):
@@ -73,8 +94,12 @@ def format_leechers(text):
     return green(text) if num(text) > 20 else red(text)
 
 def new_search(content):
-    #We need to get data in the following format:
-    #   TorrentName - Size - Age - Seed - Leech
+    '''
+    Performs a new search with given tags.
+    Builds objects with following info:
+        TorrentName - Size - Age - Seed - Leech
+    Associated with the command "--search"
+    '''
     global al
     al = [tag.get_text() for tag in content.find_all('td',{'class':'center'})]
     global href
@@ -99,22 +124,27 @@ def new_search(content):
         size = size[idx]
         id = str(idx+1)
         table.add_row([id, rowTitle, size, age, rowSeeder, rowLeecher])
+        #if idx < 15:
         #the creation
         torr = Torrent(id, title[idx] , size, age, rowSeeder, rowLeecher)
         #into dict
         torrents_found[idx] = torr
     return table
 
-def download_torr(cmdParts):
-    torrFile = requests.get(href[int(cmdParts[1])])
+'''
+DEPRECATED
+def download_torr(torr_id):
+    #torrFile = requests.get(href[int(cmdParts[1])])
     try:
+        torrFile = requests.get(href[torr_id])
         torrFile.raise_for_status()
     except Exception:
-        print('Download was not completed..')
-    actualTorrentFile = open(title[int(cmdParts[1])] + '.torrent', 'wb')
+        print('Download could not be completed..')
+        return
+    actualTorrentFile = open(title[torr_id] + '.torrent', 'wb')
     for chunk in torrFile.iter_content(100000):
         actualTorrentFile.write(chunk)
-
+'''
 def open_web_page():
     '''
     Open the webpage that shows the entire search
@@ -128,14 +158,15 @@ def open_web_page():
 def list_all_cmds():
     '''
     Shows all options.
+    Associated with the command "--list".
     '''
     print('--search {tags 1..+} -> New search with specified tags.')
-    print('--download [id] -> List all commands.')
+    print('--download [id] -> Download the torrent file for the ID given.')
     print('--webpage       -> Open the webpage with the entire results.')
     print('--exit          -> Exit application.')
     print('--list          -> List all commands.')
     print('--id [id]       -> More info about a specific torrent.')
-    
+
 
 def register_cmds():
     '''
@@ -147,8 +178,9 @@ def register_cmds():
     parser.add_argument('--search', '-s', nargs = '+', help='Starts a new search with given keywords.')
     parser.add_argument('--webpage','-webp', action = 'store_true', help = 'Open the webpage with the entire results.')
     parser.add_argument('--id', nargs = 1, help = 'More information about a given torrent ID.')
+    parser.add_argument('--download', '-d', nargs = 1, help = 'Download the torrent file for the ID given.')
     return parser
-    
+
 def cmd(cmd_line, parser):
     '''
     Interpreter of all cmds
@@ -166,8 +198,11 @@ def cmd(cmd_line, parser):
         open_web_page()
     elif args.id and len(args.id) == 1:
         print(torrents_found[num(args.id[0])-1])
-        
-    
+    elif args.download and len(args.download) == 1:
+        #download_torr(num(args.download[0]))
+        d = AsyncDownload(num(args.download[0]))
+        d.start()
+
 #script part
 wantsToExit = False
 parser = register_cmds()
